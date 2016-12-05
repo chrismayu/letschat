8837255e1536f8ca76797a6f7b5de1b6cc7dfdfa
chrismayu <chrismayu@gmail.com>
2016-12-05 11:10:51 -0500

    icon and index max

--- app/fabrication/fabrication.rb
+++ app/fabrication/fabrication.rb
@@ -1,7 +1,1 @@
-Fabricator(:user) do
-  username { Faker::Internet.user_name }
-  name { Faker::Name.first_name }
-  surname { Faker::Name.last_name }
-  password { "testtest" }
-  password_confirmation { |attrs| attrs[:password] }
-end
+ 
