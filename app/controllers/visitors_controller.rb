ec8d2390e5b230c25e8ad37288463a9fe259e7f5
chrismayu <chrismayu@gmail.com>
2016-12-20 11:28:53 -0500

    search added

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -12,6 +12,17 @@ class VisitorsController < ApplicationController
   end
   
   
+  
+  def search
+    
+    @blank_form = BlankForm.new
+    @users = User.front_page
+    
+     query = params[:q].presence || "*"
+     @users = User.search(query, suggest: true, limit: 200)
+    
+  end
+  
 
   def autocomplete
     render json: User.search(params[:term], fields: [{name: :text_start}], limit: 10).map(&:name)
